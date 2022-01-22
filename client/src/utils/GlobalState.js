ae8f458d6ece1104ecfab591a9dc5fe2c1a4c1f5
NuminousBlue <jdiehl2236@gmail.com>
2022-01-22 00:13:40 -0600

    attempting to keep context build functional while writing mirror redux code

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,9 +1,25 @@
+// import { createStore } from 'redux';
 import React, { createContext, useContext } from "react";
-import { useProductReducer } from './reducers'
+import { useProductReducer } from './reducers';
+
+
+// redux provider acts as globalized state, exposing important user data across the entire application
+
+// STORE => ACTION => REDUCER
+
+// ACTION = (Usually) simple operation that returns an object
+   //  const action = () => { return { ...state, newState}   }
+// 
+
+// REDUCER = Pure functions, often based on a switch, that return a new object combining the previous state with the updated state.
+// REDUCER = accepts the current state and an action. It returns a new state based on that action.
+
+
+// const reduxStore = createStore(useProductReducer)
+// const { Provider } = reduxStore
 
 const StoreContext = createContext();
 const { Provider } = StoreContext;
-
 const StoreProvider = ({ value = [], ...props }) => {
   const [state, dispatch] = useProductReducer({
     products: [],
@@ -17,7 +33,7 @@ const StoreProvider = ({ value = [], ...props }) => {
 };
 
 const useStoreContext = () => {
-  return useContext(StoreContext);
-};
-
-export { StoreProvider, useStoreContext };
+   return useContext(StoreContext);
+ };
+ 
+ export { StoreProvider, useStoreContext };
